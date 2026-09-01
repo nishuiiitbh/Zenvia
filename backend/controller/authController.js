f8c4295f949e43bbb6507bb69d9b496e8b4b9aeb
Nishu Kumari <kumarinishuiiitbh@gmail.com>
2026-09-01 14:31:42 +0530

    feat: set up frontend with React and Vite
    - Added React app structure and main entry point
    - Configured Vite for frontend development
    - Added initial frontend configuration files

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -35,3 +35,40 @@ export const registration = async (req,res) => {
   }
     
 }
+export const login = async (req,res) => {
+    try {
+        let {email,password} = req.body;
+        let user = await User.findOne({email}) 
+        if(!user){
+            return res.status(404).json({message:"User is not Found"})
+        }
+        let isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({message:"Incorrect password"})
+        }
+        let token = await genToken(user._id)
+        res.cookie("token",token,{
+        httpOnly:true,
+        secure:false,
+        sameSite: "Strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+    return res.status(201).json(user)
+
+    } catch (error) {
+         console.log("login error")
+    return res.status(500).json({message:`Login error ${error}`})
+        
+    }
+    
+}
+export const logOut = async (req,res) => {
+try {
+    res.clearCookie("token")
+    return res.status(200).json({message:"logOut successful"})
+} catch (error) {
+    console.log("logOut error")
+    return res.status(500).json({message:`LogOut error ${error}`})
+}
+    
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { registration } from "../controller/authController.js";
+import { registration,login,logOut } from "../controller/authController.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/registration", registration);
+authRoutes.post("/login", login);
+authRoutes.post("/logout", logOut);
 
 export default authRoutes;
