e29eeb426535f4b4b13f23789e6c660a8467596b
Nishu Kumari <kumarinishuiiitbh@gmail.com>
2026-09-12 03:48:16 +0530

    Add cart routes and APIs for get add and update

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/authRoutes.js'
 import cors from"cors"
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
+import cartRoutes from './routes/cartRoutes.js'
 
 dotenv.config()
 
@@ -23,6 +24,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/product", productRoutes)
+app.use("/api/cart", cartRoutes)
 
 
 app.listen(port, () => {
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,44 +1,43 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { authDataContext } from './authContext'
-import axios from 'axios'
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { authDataContext } from "./authContext";
+import axios from "axios";
 
-export const userDataContext = createContext()
-function UserContext({children}) {
-    let [userData,setUserData] = useState("")
-    let {serverUrl} = useContext(authDataContext)
+export const userDataContext = createContext();
+function UserContext({ children }) {
+  let [userData, setUserData] = useState("");
+  let { serverUrl } = useContext(authDataContext);
 
+  const getCurrentUser = async () => {
+    try {
+      let result = await axios.get(serverUrl + "/api/user/getcurrentuser", {
+        withCredentials: true,
+      });
 
-   const getCurrentUser = async () => {
-        try {
-            let result = await axios.get(serverUrl + "/api/user/getcurrentuser",{withCredentials:true})
-
-            setUserData(result.data)
-            console.log(result.data)
-
-        } catch (error) {
-            setUserData(null)
-            console.log(error)
-        }
+      setUserData(result.data);
+      console.log(result.data);
+    } catch (error) {
+      setUserData(null);
+      console.log(error);
     }
+  };
 
-    useEffect(()=>{
-     getCurrentUser()
-    },[])
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
 
+  let value = {
+    userData,
+    setUserData,
+    getCurrentUser,
+  };
 
-
-    let value = {
-     userData,setUserData,getCurrentUser
-    }
-    
-   
   return (
     <div>
       <userDataContext.Provider value={value}>
         {children}
       </userDataContext.Provider>
     </div>
-  )
+  );
 }
 
-export default UserContext
+export default UserContext;
