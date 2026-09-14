5767e73e25cb13ba923a3f4c8a7b014e2e411286
Nishu Kumari <kumarinishuiiitbh@gmail.com>
2026-09-15 03:43:42 +0530

    Update Vite configuration

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server:{port :5174}
+  // server:{port :5174}
 })
